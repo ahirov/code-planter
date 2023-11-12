fb08956d57c25f1e3bb796df9d7192d723b0345d
Anton Hirov <hirov.anton@yahoo.com>
2023-11-12 03:55:44 +0200

    Added Core and Tests projects. Added .gitignore file

---
+++ Tests/Core/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CodePlanter.Core.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
